ebc72cc2a23cf0a2766dfd34e760aaa1e2fd7cf1
Ethan P <pfieth16@gmail.com>
2026-05-01 14:06:31 -0500

    Redirect to Auth Page

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.currentUser()) {
       return true;
     }
-    this.router.navigate(['/login']);
+    this.router.navigate(['/sign-in']);
     return false;
   }
 }
